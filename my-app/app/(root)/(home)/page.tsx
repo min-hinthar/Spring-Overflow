74d2caeb9a8a020b7c4767a7df76d72b62820efb
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-21 20:47:30 -0800

    Layout.tsx created with imported Navbar component with Global Search, Theme and Clerk Icon

--- my-app/app/(root)/(home)/page.tsx
+++ my-app/app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
- 
 export default function Home() {
   return (
     <div className="h-screen">
-      <UserButton afterSignOutUrl="/"/>
+      
     </div>
   )
 }
