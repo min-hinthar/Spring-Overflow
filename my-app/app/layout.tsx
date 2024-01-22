87bd13b5619021cfe705475225462f643baee5f5
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-21 18:55:16 -0800

    ThemeProvider.tsx created for implementing light/dark mode

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {createContext} from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter, Space_Grotesk } from 'next/font/google'
 import type { Metadata } from 'next'
+
 import './globals.css'
+import { ThemeProvider } from '../context/ThemeProvider'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -29,19 +31,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 'primary-text-gradient hover:text-primary-500'
-        }
-      }}
-    >
-      <html lang="en">
+    <html lang="en">
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
+          <ClerkProvider
+            appearance={{
+              elements: {
+                formButtonPrimary: 'primary-gradient',
+                footerActionLink: 'primary-text-gradient hover:text-primary-500'
+              }
+            }}
+          >
+            <ThemeProvider>
+              {children}
+            </ThemeProvider>
+          </ClerkProvider> 
         </body>
       </html>
-    </ClerkProvider>
   )
 }
