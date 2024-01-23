0c19d4334a782524908d91c4f446e21ee904b70c
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-22 17:50:28 -0800

    LeftSidebar.tsx implemented with sidebarLinks, text max-lg:hidden

--- my-app/app/(root)/layout.tsx
+++ my-app/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSidebar from '@/components/shared/LeftSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
 
@@ -8,7 +9,7 @@ const Layout = ({ children }: {children: React.ReactNode }) => {
         >
             <Navbar />
             <div className='flex'>
-                LeftSidebar
+                <LeftSidebar />
 
                 <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
                     <div className='mx-auto w-full max-w-5xl'>
