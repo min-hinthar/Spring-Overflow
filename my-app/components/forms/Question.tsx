9106b4b661f70baa6008929290b612cdb07429ce
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-24 16:33:55 -0800

    tag input functionality implemented with handle input key down and handle remove tag functions

--- my-app/components/forms/Question.tsx
+++ my-app/components/forms/Question.tsx
@@ -19,6 +19,8 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 import { QuestionsSchema } from "@/lib/actions/validations"
+import { Badge } from "../ui/badge";
+import Image from "next/image";
 
 const Question = () => {
 
@@ -42,6 +44,38 @@ const Question = () => {
 
 // Tiny.Cloud.React Editor
     const editorRef = useRef(null);
+
+    const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field: any) => {
+        if (e.key === "Enter" && field.name === 'tags') {
+            e.preventDefault();
+
+            const tagInput = e.target as HTMLInputElement;
+            const tagValue = tagInput.value.trim();
+            if (tagValue !== '') {
+                if(tagValue.length > 15){
+                    return form.setError('tags', {
+                        type: 'required',
+                        message: 'Tag must be less than 15 characters!'
+                    })
+                }
+
+                if(!field.value.includes(tagValue as never)) {
+                    form.setValue('tags', [...field.value, tagValue])
+                    tagInput.value=''
+                    form.clearErrors('tags');
+                }
+            } else {
+                form.trigger();
+            }
+            
+            return;
+    }}
+
+    const handleTagRemove = (tag: string, field: any) => {
+        const newTags = field.value.filter((t: string) => t !== tag);
+        
+        form.setValue('tags', newTags);
+    }
 
     return (
     <Form {...form}>
@@ -118,11 +152,35 @@ const Question = () => {
                     Tags <span className="text-primary-500">*</span>
                 </FormLabel>
                 <FormControl className="nt-3.5">
-                    <Input className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border" 
-                        placeholder="Add Tags!"
-                    {...field}
-                    />
-                </FormControl>
+                {/* Need React Fragment to render React element child */}
+                    <>
+                        <Input className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border" 
+                            placeholder="Add Tags!"
+                            onKeyDown={(e) => handleInputKeyDown(e, field)}
+                        />
+                        {field.value.length > 0 && ( 
+                            <div className="flex-start mt-2.5 gap-2.5">
+                                {field.value.map((tag: any) => (
+                                    <Badge 
+                                        key={tag}
+                                        className="subtle-medium background-light800_dark400 text-primary-500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                                        onClick={() => handleTagRemove(tag, field)}
+                                    >
+
+                                        {tag}
+                                        <Image
+                                            src='/assets/icons/close.svg'
+                                            alt="Close icon"
+                                            width={12}
+                                            height={12}
+                                            className="cursor-pointer object-contain invert-0 dark:invert"
+                                        />
+                                    </Badge>
+                                ))}
+                            </div>
+                        )}
+                    </>
+                    </FormControl>
             <FormDescription className="body-regular mt-2.5 text-light-500">
                 Add up to 3 tags that relates to your question. Press enter to add a tag.
             </FormDescription>
