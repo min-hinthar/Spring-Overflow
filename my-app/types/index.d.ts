2f8d67953a79ab6ee377aff37a9ef362957c5bf7
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-25 12:43:54 -0800

    types/index.d.ts updated with SidebarLink exports

--- my-app/types/index.d.ts
+++ my-app/types/index.d.ts
@@ -1,73 +1,48 @@
-import { sidebarLink } from "@/types";
+import { BADGE_CRITERIA } from "@/constants";
 
-export const themes = [
-  { value: "light", label: "Light", icon: "/assets/icons/sun.svg" },
-  { value: "dark", label: "Dark", icon: "/assets/icons/moon.svg" },
-  { value: "system", label: "System", icon: "/assets/icons/computer.svg" },
-];
+export interface SidebarLink {
+  imgURL: string;
+  route: string;
+  label: string;
+}
 
-export const sidebarLinks: SidebarLink[] = [
-  {
-    imgURL: "/assets/icons/home.svg",
-    route: "/",
-    label: "Home",
-  },
-  {
-    imgURL: "/assets/icons/users.svg",
-    route: "/community",
-    label: "Community",
-  },
-  {
-    imgURL: "/assets/icons/star.svg",
-    route: "/collection",
-    label: "Collections",
-  },
-  {
-    imgURL: "/assets/icons/suitcase.svg",
-    route: "/jobs",
-    label: "Find Jobs",
-  },
-  {
-    imgURL: "/assets/icons/tag.svg",
-    route: "/tags",
-    label: "Tags",
-  },
-  {
-    imgURL: "/assets/icons/user.svg",
-    route: "/profile",
-    label: "Profile",
-  },
-  {
-    imgURL: "/assets/icons/question.svg",
-    route: "/ask-question",
-    label: "Ask a question",
-  },
-];
+export interface Job {
+  id?: string;
+  employer_name?: string;
+  employer_logo?: string | undefined;
+  employer_website?: string;
+  job_employment_type?: string;
+  job_title?: string;
+  job_description?: string;
+  job_apply_link?: string;
+  job_city?: string;
+  job_state?: string;
+  job_country?: string;
+}
 
-export const BADGE_CRITERIA = {
-  QUESTION_COUNT: {
-    BRONZE: 10,
-    SILVER: 50,
-    GOLD: 100,
-  },
-  ANSWER_COUNT: {
-    BRONZE: 10,
-    SILVER: 50,
-    GOLD: 100,
-  },
-  QUESTION_UPVOTES: {
-    BRONZE: 10,
-    SILVER: 50,
-    GOLD: 100,
-  },
-  ANSWER_UPVOTES: {
-    BRONZE: 10,
-    SILVER: 50,
-    GOLD: 100,
-  },
-  TOTAL_VIEWS: {
-    BRONZE: 1000,
-    SILVER: 10000,
-    GOLD: 100000,
-  },
-};
+export interface Country {
+  name: {
+    common: string;
+  };
+}
+
+export interface ParamsProps {
+  params: { id: string };
+}
+
+export interface SearchParamsProps {
+  searchParams: { [key: string]: string | undefined };
+}
+
+export interface URLProps {
+  params: { id: string };
+  searchParams: { [key: string]: string | undefined };
+}
+
+export interface BadgeCounts {
+  GOLD: number;
+  SILVER: number;
+  BRONZE: number;
+}
+
+export type BadgeCriteriaType = keyof typeof BADGE_CRITERIA;
