400f13baa98a28a5133079da3b56989c8838f0bd
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-25 16:18:02 -0800

    middleware.ts updated for publicRoutes and ignoredRoutes, successfully deployed to Vercel after whitelisting IP address on MongoDB

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -4,12 +4,21 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: [
-        '/',
-        "/api/webhooks(.*)",
-    ]
+  publicRoutes: [
+    '/',
+    '/api/webhook',
+    '/question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs'
+  ],
+  ignoredRoutes: [
+    '/api/webhook', '/api/chatgpt'
+  ]
 });
  
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
