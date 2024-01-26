a0eaa9dae1e277b4de4645ee2527dacd2c5654b4
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-25 18:30:49 -0800

    QuestionCard updated to render image Url author.name, next configjs updated with remote patterns for img.clerk.com

--- my-app/components/cards/QuestionCard.tsx
+++ my-app/components/cards/QuestionCard.tsx
@@ -62,7 +62,7 @@ const QuestionCard = ({
 
         <div className='flex-between mt-6 w-full flex-wrap gap-3'>
             <Metric
-                imgUrl='/assets/icons/avatar.svg'
+                imgUrl={author.picture}
                 alt='User'
                 value={author.name}
                 title={` - asked ${getTimestamp(createdAt)}`}
--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*',
+            },
+            {
+                protocol: 'http',
+                hostname: 'img.clerk.com',
+            },
+        ]
+    }
+};
 
 export default nextConfig;
