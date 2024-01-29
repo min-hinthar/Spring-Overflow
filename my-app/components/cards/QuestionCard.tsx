b4b7a17c20939f59c36326582211d3202567cfa4
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-28 22:34:18 -0800

    Edit Delete Action component implemented on Question Card and Answer Card using delete actions

--- my-app/components/cards/QuestionCard.tsx
+++ my-app/components/cards/QuestionCard.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import RenderTag from '../shared/RenderTag'
 import Metric from '../shared/Metric'
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils'
+import { SignedIn } from '@clerk/nextjs'
+import EditDeleteAction from '../shared/EditDeleteAction'
 
 
 interface QuestionProps {
@@ -36,6 +38,9 @@ const QuestionCard = ({
     answers,
     createdAt,
 }: QuestionProps) => {
+
+    const showActionButtons = clerkId && clerkId === author.clerkId;
+
   return (
     <div className='card-wrapper p-9 sm:px-11 rounded-[10px]'>
         <div className='flex flex-col-reverse items-start justify-between gap-5 sm:flex-row'>
@@ -50,7 +55,15 @@ const QuestionCard = ({
                 </Link>
             </div>
 
-            {/* If signed-in, add edit delete action */}
+            <SignedIn>
+                {showActionButtons && (
+                    <EditDeleteAction 
+                        type='Question'
+                        itemId={JSON.stringify(_id)}
+                    />
+                )}
+            </SignedIn>
+
         </div>
 
         <div className='mt-3.5 flex flex-wrap gap-2'>
