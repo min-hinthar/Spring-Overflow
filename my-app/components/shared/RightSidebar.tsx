85ca5732299b320924c80ade640ad4ae29baff11
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-29 14:53:14 -0800

    Hot questions feature implemented with getHotQuestions action on RighSidebar

--- my-app/components/shared/RightSidebar.tsx
+++ my-app/components/shared/RightSidebar.tsx
@@ -2,24 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import RenderTags from './RenderTag'
-
-const hotQuestions = [
-    {
-        _id: '1', title: 'How do I use express as a custom server in NextJS?'
-    },
-    {
-        _id: '2', title: 'Cascading Deletes in SQL Alchemy?'
-    },
-    {
-        _id: '3', title: 'How to Perfectly Center a Div with Tailwind CSS?'
-    },
-    {
-        _id: '4', title: 'Best practices for data fetching in a NextJS application with Server-Side Rendering (SSR)?'
-    },
-    {
-        _id: '5', title: 'Redux Toolkit Not Updating State as Expected?'
-    },
-]
+import { getHotQuestions } from '@/lib/actions/question.action'
 
 const popularTags = [
     {
@@ -39,7 +22,9 @@ const popularTags = [
     },
 ]
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+    const hotQuestions = await getHotQuestions();
+
 
   return (
     <section className='background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden w-[350px] custom-scrollbar'>
