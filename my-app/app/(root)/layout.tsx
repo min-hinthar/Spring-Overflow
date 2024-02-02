844aab933a5f01fb400839ee53752e702257b1a7
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-02-01 17:03:26 -0800

    Toast ShadCN UI component installed and implemented on upvote/downvote questions and answers

--- my-app/app/(root)/layout.tsx
+++ my-app/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import LeftSidebar from '@/components/shared/LeftSidebar'
 import RightSidebar from '@/components/shared/RightSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
+import { Toaster } from '@/components/ui/toaster'
 import React from 'react'
 
 const Layout = ({ children }: {children: React.ReactNode }) => {
@@ -21,7 +22,7 @@ const Layout = ({ children }: {children: React.ReactNode }) => {
                 <RightSidebar />
             </div>
 
-            Toaster
+            <Toaster />
     </main>
   )
 }
